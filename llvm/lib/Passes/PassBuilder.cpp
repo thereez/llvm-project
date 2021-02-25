6fd93f77bd812f6a331d969ed7b5693a70a123fd
Sasha Vokhmyanina <al.vokhmyanina@yandex.ru>
2021-02-25 12:53:10 +0300

    Lab1 introduce VokhmyaninaCounter

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -237,6 +237,7 @@
 #include "llvm/Transforms/Vectorize/LoopVectorize.h"
 #include "llvm/Transforms/Vectorize/SLPVectorizer.h"
 #include "llvm/Transforms/Vectorize/VectorCombine.h"
+#include "llvm/Transforms/VokhmyaninaCounter/VokhmyaninaCounter.h"
 
 using namespace llvm;
 
