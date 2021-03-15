de653716397607518d7f658339411a717e044651
kovakimy <kristina.ovakimyan@intel.com>
2021-03-15 15:32:05 +0300

    [LW2] Separated lab1 and lab2

--- llvm/lib/Transforms/KovakimyCount/KovakimyCount.cpp
+++ llvm/lib/Transforms/KovakimyCount/KovakimyCount.cpp
@@ -11,8 +11,6 @@ STATISTIC(TotalArithmInstr, "Number of arithmetic instructions");
 STATISTIC(CounterAdd, "Number of adds");
 STATISTIC(CounterMult, "Number of muls");
 
-#include "llvm/IR/InstIterator.h"
-#include "llvm/ADT/SmallVector.h"
 using namespace llvm;
 
 void counterLoop(Loop* loop)
@@ -24,7 +22,7 @@ void counterLoop(Loop* loop)
 
 PreservedAnalyses KovakimyCountPass::run(Function &F, FunctionAnalysisManager &AM) 
 {
-  /*if (!F.isDeclaration())
+  if (!F.isDeclaration())
   {
 	  TotalFuncs++;
 	  llvm::LoopAnalysis::Result& loop = AM.getResult<LoopAnalysis>(F);
@@ -41,82 +39,8 @@ PreservedAnalyses KovakimyCountPass::run(Function &F, FunctionAnalysisManager &A
 				  CounterMult++;
 		  }
 	  }
-  }*/
+  }
  
-    auto pa = PreservedAnalyses::all();
-    SmallVector<BinaryOperator *, 16> wark_set_l;
-    SmallVector<BinaryOperator *, 16> wark_set_r;
-    SmallVector<CallBase *, 16> wark_set_pow;
-     
-    for (Instruction &I : instructions(F))
-    {
-        if (BinaryOperator *BO = dyn_cast<BinaryOperator>(&I)) 
-        {
-            if (ConstantFP *RHSC = dyn_cast<ConstantFP>(BO->getOperand(1))) 
-            {
-                if (RHSC->getValue().convertToFloat() == 0.f && BO->getOpcode() == Instruction::FAdd) 
-                {
-                    wark_set_r.push_back(BO);
-                    pa = PreservedAnalyses::none();
-                }
-                if (RHSC->getValue().convertToFloat() == 1.f && BO->getOpcode() == Instruction::FMul) 
-                {
-                    wark_set_r.push_back(BO);
-                    pa = PreservedAnalyses::none();
-                }
-        } else if (ConstantFP *RHSC = dyn_cast<ConstantFP>(BO->getOperand(0))) 
-        {
-            if (RHSC->getValue().convertToFloat() == 0.f &&  BO->getOpcode() == Instruction::FAdd) 
-            {
-                wark_set_l.push_back(BO);
-                pa = PreservedAnalyses::none();
-            }
-            if (RHSC->getValue().convertToFloat() == 1.f &&  BO->getOpcode() == Instruction::FMul) 
-            {
-                wark_set_l.push_back(BO);
-                pa = PreservedAnalyses::none();
-            }
-        }
-    }
-        if (CallInst *C = dyn_cast<CallInst>(&I)) 
-        {
-            if (C->getCalledFunction()->getName() == "powf") 
-            {
-                if (ConstantFP *FPV = dyn_cast<ConstantFP>(C->getOperand(1))) 
-                {
-                    if (FPV->getValue().convertToFloat() == 2.f) 
-                    {
-                        wark_set_pow.push_back(C);
-                        pa = PreservedAnalyses::none();
-                    }
-                }
-            }
-        }
-    }
-
-    while (!wark_set_r.empty()) 
-    {
-        BinaryOperator *BO = wark_set_r.pop_back_val();
-        BO->replaceAllUsesWith(BO->getOperand(0));
-        BO->eraseFromParent();
-    }
-
-    while (!wark_set_l.empty()) 
-    {
-        BinaryOperator *BO = wark_set_l.pop_back_val();
-        BO->replaceAllUsesWith(BO->getOperand(1));
-        BO->eraseFromParent();
-    }
-
-    while (!wark_set_pow.empty()) 
-    {
-        CallBase *CB = wark_set_pow.pop_back_val();
-        BinaryOperator* BO_MUL = BinaryOperator::Create(Instruction::FMul, CB->getOperand(0), CB->getOperand(0));
-        CB->replaceAllUsesWith(BO_MUL);
-        BO_MUL->insertBefore(CB);
-        CB->eraseFromParent();
-    }
-
-    //return PreservedAnalyses::all();
-    return pa;
+    return PreservedAnalyses::all();
+    
 }
